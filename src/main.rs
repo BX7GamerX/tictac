0e5d47e6beb9db4242fe9c8219d7c0cf2a8b0d73
him-linux <bwires947@gmail.com>
2024-12-13 22:19:17 +0300

    rand ai

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 mod output;
 use output::position_to_index;
+use output::Game;
 use output::Table;
 use output::Player;
 fn main() {
-    let mut tictac_board = Table::new();
-    tictac_board.init();
-    let mut player1 = Player::new("Player 1".to_string(), 'X');
-    loop {
-    let mut input = output::get_int("");
-    let mut count = 0;
-    player1.play(&mut tictac_board, input);
-    if tictac_board.check_full() {
-        println!("It's a draw!");
-        break;
-    }
-    count += 1;
-    }
+    let mut tictac_game = output::Game::new();
+    tictac_game.play();
 }
--- src/output.rs
+++ src/output.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub struct Cell {
     pub owner: String,
     pub symbol: char,
@@ -31,19 +33,70 @@ pub fn position_to_index(position: i32) -> i32 {
 pub struct Table{
     cells: Vec<Cell>,
     full : bool,
-    winning_combo: Vec<Cell>,
+    //winning_combo: Vec<Cell>,
     play_count : i32,
+    winning_combo: [[usize;3]; 8],
 }
 
+/// Creates a new `Table` instance with default values.
+
+/// Returns the list of winning combinations that include the given cell index.
+
+/// Checks if the given player has won after making a move at the specified index.
+
+/// Initializes the `Table` for a new game.
+
+/// Retrieves a reference to the `Cell` at the specified index.
+
+/// Clears the console and prints the current state of the table.
+
+/// Processes a player's move at the specified index.
+
+/// Checks if the table is full (i.e., no more moves can be made).
 impl Table{
     pub fn new() -> Table {
         let mut cells_in = (0..9).map(|i| Cell::new(String::new(), ' ', false, i, i, false)).collect();
         Table {
             cells: cells_in,
             full: false,
-            winning_combo: Vec::new(),
+            winning_combo : [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6],
+            ],
             play_count: 0,
         }
+    }
+    fn get_relevant_list (&self, index : i32 ) -> Vec<[usize;3]>{
+        let mut relevant_list = Vec::new();
+        for combo in self.winning_combo.iter(){
+            if combo.contains(&(index as usize)){
+                relevant_list.push(*combo);
+            }
+        }
+        relevant_list
+    }
+    fn check_winner(&mut self, player: &Player , index : i32) -> bool {
+        for combo in self.get_relevant_list(index) {
+            let mut count = 0;
+            for cell in combo.iter() {
+                if self.cells[*cell].owner == player.name {
+                    count += 1;
+                }
+            }
+            if count == 3 {
+                for cell in combo.iter() {
+                    self.cells[*cell].winning_cell = true;
+                }
+                return true;
+            }
+        }
+        false
     }
     pub fn init (&mut self){
         let mut count = 0;
@@ -94,11 +147,21 @@ impl Table{
         if self.check_full(){
             return;
         };
+
+        self.place_cell(player, index);
+
+    }
+    fn place_cell (&mut self, player: &mut Player, index: i32){
+
         self.cells[index as usize].owner = player.name.clone();
         self.cells[index as usize].symbol = player.symbol.clone();
         self.cells[index as usize].is_occupied = true;
         self.print();
         self.play_count += 1;
+        if self.check_winner(player, index){
+            println!("{} wins!", player.name);
+            self.full = true;
+        };
     }
     pub fn check_full (&mut self) -> bool{
         if self.play_count > 8 {
@@ -142,3 +205,93 @@ pub fn get_int (message: &str) -> i32 {
         }
     }
 }
+fn  get_string (message: &str) -> String {
+    loop {
+        println!("{}", message);
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim().to_string();
+        if input.is_empty() {
+            println!("Invalid input");
+        } else {
+            return input;
+        }
+    }
+}
+fn get_char (message: &str) -> char {
+    loop {
+        println!("{}", message);
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim().to_string();
+        if input.is_empty() || input.chars().count() > 1 {
+            println!("Invalid input");
+        } else {
+            return input.chars().next().unwrap();
+        }
+    }
+}
+pub struct Game {
+    pub tictac_board: Table,
+    pub player1: Player,
+    pub player2: Player,
+    pub player1_moves: Vec<i32>,
+    pub player2_moves: Vec<i32>,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        let mut tictac_board = Table::new();
+        tictac_board.init();
+        let mut player1 = Player::new(
+            get_string("Enter player 1 name"), 
+            get_char("Choose symbol for player 1"),);
+        let mut player2 = Player::new(
+            get_string("Enter Player two name"), 
+            get_char("Choose symbol for player 2"),);
+        Game {
+            tictac_board,
+            player1,
+            player2,
+            player1_moves: Vec::new(),
+            player2_moves: Vec::new(),
+        }
+    }
+    pub fn ai_play_move(&mut self)-> i32{
+        let mut rng = rand::thread_rng();
+        let mut ai_move = rng.gen_range(1..10);
+        while self.player1_moves.contains(&ai_move) || self.player2_moves.contains(&ai_move) {
+            ai_move = rng.gen_range(1..10);
+        }
+        ai_move
+
+    }
+    pub fn play(&mut self){
+        let mut iterator = 0;
+        loop {
+            let input = if (iterator == 0 && self.player1.is_ai) || (iterator == 1 && self.player2.is_ai) {
+                let ai_move = self.ai_play_move();
+                ai_move
+            } else {
+                get_int("Enter a number between 1 and 9")
+            };
+
+            if iterator == 0 {
+                self.player1.play(&mut self.tictac_board, input);
+                self.player1_moves.push(input);
+            } else {
+                self.player2.play(&mut self.tictac_board, input);
+                self.player2_moves.push(input);
+            }
+
+            if self.tictac_board.check_full() {
+                    println!("It's a draw!");
+                
+                break;
+            }
+
+            iterator = if iterator == 0 { 1 } else { 0 };
+        }
+    }
+}
+
