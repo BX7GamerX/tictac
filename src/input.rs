cc67905bca76c94404e6ec13e352f7b56f420ca9
him-linux <bwires947@gmail.com>
2025-01-05 17:22:16 +0300

    able to read from csv

--- src/input.rs
+++ src/input.rs
@@ -1,9 +1,13 @@
+use csv::ReaderBuilder;
+use std::{error::Error, string};
+
+#[derive(Clone)]
 pub struct GameData {
     pub winner: String,
     pub player1: String,
     pub player2: String,
     pub state_of_cells_list: Vec<[i8;9]>,
-    pub periodic_State_of_cells : [i8;9],
+    pub periodic_state_of_cells : [i8;9],
 }
 impl GameData {
     pub fn new(player1: String, player2: String) -> GameData {
@@ -12,8 +16,14 @@ impl GameData {
             player1,
             player2,
             state_of_cells_list : Vec::new(),
-            periodic_State_of_cells : [0,0,0,0,0,0,0,0,0],
+            periodic_state_of_cells: [0;9],
         }
+    }
+    pub fn get_round_State (&self, index: usize) -> [i8;9] {
+        if index >= self.state_of_cells_list.len() {
+            return self.state_of_cells_list[self.state_of_cells_list.len()-1];
+        }
+        self.state_of_cells_list[index]
     }
 }
 
@@ -32,7 +42,69 @@ impl GamesData {
     pub fn add_game(&mut self, game_data: GameData) {
         self.game_data.push(game_data);
     }
-    pub fn read_datat(&self) {
-        println!("Reading data from file: {}", self.csv_file);
+    pub fn get_game(&self, index: usize) -> GameData {
+        self.game_data[index].clone()
+    }
+    pub fn print_game(&self, index: usize) {
+        let game = self.get_game(index);
+        println!("Winner: {}", game.winner);
+        println!("Player 1: {}", game.player1);
+        println!("Player 2: {}", game.player2);
+        println!("---------------------------------");
+        for state in game.state_of_cells_list.iter(){
+            for cell in state.iter(){
+                print!("{} ", cell);
+            }
+            println!();
+        }
+    }
+    // the glory code please don't touch it
+    pub fn read_data(&mut self) {
+        let reader = ReaderBuilder::new()
+            .has_headers(false)
+            .from_path(&self.csv_file);
+        match reader {
+            Ok(mut reader) => {
+                let mut temp_game_data = GameData::new("ai".to_string(),"ai_2".to_string());
+                for result in reader.records(){
+                    match result {
+                        Ok(record) =>{
+                            let mut index = 0;
+                            for item in record.iter(){
+                                match item{
+                                    "-1"|"0"|"1" => {
+                                        temp_game_data.periodic_state_of_cells[index] = item.parse::<i8>().unwrap();
+                                        index += 1;
+                                    }
+                                    "" => {
+                                        if index >= 8 {
+                                            temp_game_data.state_of_cells_list.push(temp_game_data.periodic_state_of_cells.clone());
+                                        }
+                                        index = 0;
+                                    }
+                                    "ai"|"ai_2"|"draw" => {
+                                        temp_game_data.winner.push_str(item);
+                                        temp_game_data.state_of_cells_list.push(temp_game_data.periodic_state_of_cells);
+                                        index = 0;
+                                        self.game_data.push(temp_game_data.clone());
+                                        //if true the game ends
+                                        temp_game_data = GameData::new("ai".to_string(),"ai_2".to_string());
+                                    }
+                                    _ => {
+                                        println!("item: {}", item);
+                                    }
+                                }
+                            }
+                        }
+                        Err(error) => {
+                            println!("Error reading record: {}", error);
+                        }
+                    }
+                }
+            }
+            Err(error) => {
+                println!("Error reading file: {}", error);
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-
+mod input;
 mod output;
 mod g_class;
 mod g_ai;
@@ -8,7 +8,7 @@ mod him_network;
 fn test_game(){
     let player_type = String::from("ai_Vs_ai");
     let mut cycles_count = 0;
-    let cycles_limit = 100;//output::get_int("Enter the number of cycles to play: ");
+    let cycles_limit = 2;//output::get_int("Enter the number of cycles to play: ");
     loop {
         let mut tictac_game = output::Game::new(player_type.clone());
         tictac_game.play();
@@ -20,6 +20,9 @@ fn test_game(){
 }
 fn main() {
     test_game();
+    let mut game_data = input::GamesData::new(String::from("table.csv"));
+    game_data.read_data();
+    game_data.print_game(4);
 
 }
 
