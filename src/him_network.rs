98a31b8e082999b65d59b48d851958748d04a81a
him-linux <bwires947@gmail.com>
2025-01-05 20:44:20 +0300

    perfect init of params

--- src/him_network.rs
+++ src/him_network.rs
@@ -24,6 +24,27 @@ impl HimNetwork {
     pub fn new() -> HimNetwork {
         HimNetwork { input_nodes: HashMap::new(), output_nodes: HashMap::new() }
     }
+    pub fn init_params(self)-> ( [[f32; 9]; 10], [[f32; 10]; 10], [f32; 10], [f32; 10]) {
+        const INPUT_LAYER_SIZE :i64 = 9;
+        const HIDDEN_LAYER_SIZE :i64 = 10;
+        let mut w1 :[[f32; INPUT_LAYER_SIZE as usize]; HIDDEN_LAYER_SIZE as usize] = [[3.14159; 9]; 10] ;
+        let mut w2 :[[f32; HIDDEN_LAYER_SIZE as usize]; HIDDEN_LAYER_SIZE as usize] = [[3.14159; 10]; 10];
+        let mut b1 :[f32; HIDDEN_LAYER_SIZE as usize] = [3.14159; 10];
+        let mut b2 :[f32; HIDDEN_LAYER_SIZE as usize] = [3.14159; 10];
+
+        for layers in 0 .. HIDDEN_LAYER_SIZE {
+            for inputs in 0 .. INPUT_LAYER_SIZE {
+                w1[layers as usize][inputs as usize] = rand::thread_rng().gen_range(0.0..1.0) - 0.5;
+            }
+            for inputs in 0.. HIDDEN_LAYER_SIZE {
+                w2[layers as usize][inputs as usize] = rand::thread_rng().gen_range(0.0..1.0) - 0.5;
+            }
+            b1[layers as usize] = rand::thread_rng().gen_range(0.0..1.0) - 0.5;
+            b2[layers as usize] = rand::thread_rng().gen_range(0.0..1.0) - 0.5;
+        }
+        (w1,w2,b1,b2)
+
+    }
     pub fn add_input_node(&mut self, input_list: [i8;9]) {
         let mut index = 0;
         for data in input_list.iter() {
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod him_network;
 fn test_game(){
     let player_type = String::from("ai_Vs_ai");
     let mut cycles_count = 0;
-    let cycles_limit = 2;//output::get_int("Enter the number of cycles to play: ");
+    let cycles_limit = 200;//output::get_int("Enter the number of cycles to play: ");
     loop {
         let mut tictac_game = output::Game::new(player_type.clone());
         tictac_game.play();
@@ -18,8 +18,7 @@ fn test_game(){
         }
     }
 }
-
-fn main() {
+fn test_reading () {
     test_game();
     let mut game_data = input::GamesData::new(String::from("table.csv"));
     game_data.read_data();
@@ -30,5 +29,11 @@ fn main() {
     network.add_input_node(round_one);
     //game_data.print_game(4);
     network.print_input_nodes();
+}
+
+fn main() {
+    let test_network = him_network::HimNetwork::new();
+    let (w1,w2,b1,b2) = test_network.init_params();
+    println!("{:?}", w1[0]);
 }
 
